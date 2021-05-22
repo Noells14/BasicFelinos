93d6e4946ac6d69b955c4b4d6ec9c58656ac2d5e
Pablo Leon <pablo.lemo89@gmail.com>
2021-05-22 14:49:59 -0500

    Variables en los templates

--- src/felino.py
+++ src/felino.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
-    return render_template("home.html")
+    return render_template("home.html",titulo="Felinos - Plasma tu idea!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
