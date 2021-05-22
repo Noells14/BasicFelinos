d3f0d8c0f1fc5152d8787a03e8acadecc62f27a7
Pablo Leon <pablo.lemo89@gmail.com>
2021-05-22 15:59:20 -0500

    Proyecto basico

--- src/felino.py
+++ src/felino.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("home.html",titulo="Felinos - Plasma tu idea!")
 
+@app.route('/about', methods=["GET","POST"])
+def about():
+    return render_template("about.html",titulo="Felinos - Nosotros")
+
 if __name__ == '__main__':
     app.run(debug=True)
