59847fd74bcf0a52eab5d84ce0e3966d5faa7b75
Pablo Leon <pablo.lemo89@gmail.com>
2021-05-22 20:02:35 -0500

    Proyecto basico

--- src/felino.py
+++ src/felino.py
@@ -10,5 +10,9 @@ def index():
 def about():
     return render_template("about.html",titulo="Felinos - Nosotros")
 
+@app.route('/products', methods=["GET","POST"])
+def products():
+    return render_template("products.html",titulo="Felinos - Trabajando!")
+
 if __name__ == '__main__':
     app.run(debug=True)
