c22e079b56ca4e871719f4debb7dd0f5445eb1da
Pablo Leon <pablo.lemo89@gmail.com>
2021-05-22 22:06:55 -0500

    Proyecto basico

---
+++ felino.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET","POST"])
+def index():
+    return render_template("home.html",titulo="Felinos - Plasma tu idea!")
+
+@app.route('/about', methods=["GET","POST"])
+def about():
+    return render_template("about.html",titulo="Felinos - Nosotros")
+
+@app.route('/products', methods=["GET","POST"])
+def products():
+    return render_template("products.html",titulo="Felinos - Trabajando!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
